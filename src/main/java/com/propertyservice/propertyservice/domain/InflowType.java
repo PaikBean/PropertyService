77e75ee2cb34facaee39bfcbb67f2202a40010fa
jaehyuk <ljh@nis.co.kr>
2024-05-19 20:28:11 +0900

    [FEAT] InflowType 엔티티 추가

---
+++ src/main/java/com/propertyservice/propertyservice/domain/InflowType.java
@@ -0,0 +1,27 @@
+package com.propertyservice.propertyservice.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "inflow_type")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InflowType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inflow_type_id")
+    private Long id;
+    @Column(nullable = false)
+    private String inflowType;
+
+    @Builder
+    public InflowType(Long id, String inflowType) {
+        this.id = id;
+        this.inflowType = inflowType;
+    }
+}
