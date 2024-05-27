0ad0cb568d8393ddec982320e1cf8a57b7685951
yec3168 <yec3168@naver.com>
2024-05-27 16:38:27 +0900

    [FEAT] Manager, ManagerAddress 생성.

--- src/main/java/com/propertyservice/propertyservice/domain/company/Manager.java
+++ src/main/java/com/propertyservice/propertyservice/domain/company/Manager.java
@@ -1,4 +1,57 @@
 package com.propertyservice.propertyservice.domain.company;
 
-public class Manager {
+import com.propertyservice.propertyservice.domain.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "mamager")
+@Entity
+public class Manager extends BaseTimeEntity{
+    @Id
+    @GeneratedValue
+    @Column(name = "manager_id")
+    private Long managerId;
+    private Long department_id; // department Entity
+    private String managerName;
+    private String managerRank;
+    private String managerPosition;
+    private String managerCode;
+
+    private Long managerStateId; //stateId Entity
+    private Long genderId; // genderId;
+    private String managerPhoneNumber;
+
+    private Long managerAddressId; // addressId Entity
+
+    private LocalDateTime managerEntranceDate;
+    private LocalDateTime managerResignDate;
+    private String managerEmail; // Login Email;
+    private String managerPassword;
+    private Integer passwordErrorCount;
+
+
+    @Builder
+    public Manager(String managerName, String managerRank, String managerPosition, String managerCode, Long managerStateId, Long genderId, String managerPhoneNumber, Long managerAddressId, LocalDateTime managerEntranceDate, LocalDateTime managerResignDate, String managerEmail, String managerPassword, Integer passwordErrorCount){
+        this.managerName = managerName;
+        this.managerRank = managerRank;
+        this.managerPosition = managerPosition;
+        this.managerCode = managerCode;
+        this.managerStateId = managerStateId;
+        this.genderId = genderId;
+        this.managerPhoneNumber = managerPhoneNumber;
+        this.managerAddressId = managerAddressId;
+        this.managerEntranceDate = managerEntranceDate;
+        this.managerResignDate = managerResignDate;
+        this.managerEmail = managerEmail;
+        this.managerPassword = managerPassword;
+        this.passwordErrorCount =passwordErrorCount;
+    }
+
 }
--- src/main/java/com/propertyservice/propertyservice/domain/company/ManagerAddress.java
+++ src/main/java/com/propertyservice/propertyservice/domain/company/ManagerAddress.java
@@ -1,4 +1,32 @@
 package com.propertyservice.propertyservice.domain.company;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "manager_address")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ManagerAddress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "manager_address")
+    private Long managerAddressId;
+    @Column(nullable = false)
+    private Long addressLevel1Id;
+    @Column(nullable = false)
+    private Long addressLevel2Id;
+    @Column(nullable = false)
+    private String addressLevel3;
+
+    @Builder
+    public ManagerAddress( Long addressLevel1Id, Long addressLevel2Id, String addressLevel3){
+        this.addressLevel1Id= addressLevel1Id;
+        this.addressLevel2Id =addressLevel2Id;
+        this.addressLevel3 = addressLevel3;
+    }
 }
