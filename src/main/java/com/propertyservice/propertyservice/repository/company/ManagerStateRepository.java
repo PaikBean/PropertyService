66b7294491931aad365082763ca0e8baa6b482e8
yec3168 <yec3168@naver.com>
2024-05-30 17:26:36 +0900

    [REFACTOR] ManagerStateRepository.java 삭제

--- src/main/java/com/propertyservice/propertyservice/repository/company/ManagerStateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.propertyservice.propertyservice.repository.company;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ManagerStateRepository extends JpaRepository<ManagerState, Long> {
-}
