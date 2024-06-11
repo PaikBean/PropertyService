ce18d2f860634d964903fd1382def9eb66f508ed
jaehyuk <ljh@nis.co.kr>
2024-06-11 11:21:08 +0900

    [FIX] 회사 코드 검증 API 수정

--- src/main/java/com/propertyservice/propertyservice/controller/CompanyController.java
+++ src/main/java/com/propertyservice/propertyservice/controller/CompanyController.java
@@ -2,7 +2,6 @@ package com.propertyservice.propertyservice.controller;
 
 import com.propertyservice.propertyservice.domain.common.Response;
 import com.propertyservice.propertyservice.domain.common.ResponseCode;
-import com.propertyservice.propertyservice.repository.company.CompanyRepository;
 import com.propertyservice.propertyservice.service.CompanyService;
 import com.propertyservice.propertyservice.utils.validation.ValidBizRegNumber;
 import com.propertyservice.propertyservice.utils.validation.dto.BizNumberValidateRequestForm;
@@ -19,12 +18,17 @@ import org.springframework.web.bind.annotation.*;
 public class CompanyController {
     private final CompanyService companyService;
 
+    /**
+     * 회사 코드 검증
+     *
+     * @param companyCode
+     * @return
+     */
     @GetMapping("/v1/validate/company-code")
-    public Response searchCompanyByCompanyCode(@RequestParam(value = "companyCode", defaultValue = "") String companyCode ){
+    public Response searchCompanyByCompanyCode(@RequestParam(value = "companyCode", defaultValue = "") String companyCode) {
         try {
-            companyService.searchCompanyByCompanyCode(companyCode);
-            return new Response(ResponseCode.SUCCESS, null, "200");
-        } catch (Exception e){
+            return new Response(ResponseCode.SUCCESS, companyService.searchCompanyByCompanyCode(companyCode), "200");
+        } catch (Exception e) {
             return new Response(ResponseCode.FAIL, e.getMessage(), "400");
         }
     }
