df25137088e8e562672bc0ec64dc9dfe9e937c8b
jaehyuk <ljh@nis.co.kr>
2024-07-07 19:26:54 +0900

    [FEAT] 로그인 페이지에서 라우팅시 프로그래스바 추가

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -2,7 +2,7 @@
 import { useRouter } from 'next/navigation' // useRouter 훅을 임포트합니다.
 
 import SilhouetteLicense from '@/components/typography/SilhouetteLicense'
-import { Box, Grid, Typography, Avatar } from '@mui/material'
+import { Box, Grid, Typography, Avatar, CircularProgress } from '@mui/material'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import InputEmail from '@/components/textfield/InputEmail'
 import InputPassword from '@/components/textfield/InputPassword'
@@ -11,18 +11,23 @@ import SignUpLink from '@/components/typography/SignupLink'
 import FindPasswordLink from '@/components/typography/FindPasswordLink'
 import { useDispatch, useSelector } from 'react-redux'
 import { loginUser } from '@/store/slices/authSlice'
+import RegistCompanyLink from '@/components/typography/RegistCompanyLink'
+import { useState } from 'react'
 
 export default function Home() {
   const router = useRouter() // useRouter 훅으로 router 객체를 가져옵니다.
   const dispatch = useDispatch()
   const { user, status, error } = useSelector((state) => state.auth)
+  const [loading, setLoading] = useState(false)
 
   const handleSubmit = async (event) => {
     event.preventDefault()
+    setLoading(true)
     const data = new FormData(event.currentTarget)
     const email = data.get('email')
     const password = data.get('password')
     const resultAction = await dispatch(loginUser({ email, password }))
+    setLoading(false)
 
     if (loginUser.fulfilled.match(resultAction)) {
       router.push('/main')
@@ -31,59 +36,82 @@ export default function Home() {
     }
   }
   return (
-    <Grid container>
-      <Grid
-        item
-        xs
-        sx={{
-          position: 'relative',
-          backgroundImage: "url('/silhouette-skyline-illustration/78786.jpg')",
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          flex: 1,
-        }}
-      >
-        <SilhouetteLicense />
-      </Grid>
-      <Grid
-        item
-        sx={{
-          height: '100vh',
-          width: '500px', // 고정된 너비 값을 설정합니다
-        }}
-      >
-        <Box
+    <>
+      {' '}
+      <Grid container>
+        <Grid
+          item
+          xs
           sx={{
-            mt: '150px',
-            paddingX: '20px',
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            position: 'relative',
+            backgroundImage:
+              "url('/silhouette-skyline-illustration/78786.jpg')",
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            flex: 1,
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 2 }}>
-            <InputEmail />
-            <InputPassword />
-            <LoginBtn />
+          <SilhouetteLicense />
+        </Grid>
+        <Grid
+          item
+          sx={{
+            height: '100vh',
+            width: '500px', // 고정된 너비 값을 설정합니다
+          }}
+        >
+          <Box
+            sx={{
+              mt: '150px',
+              paddingX: '20px',
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 2 }}>
+              <InputEmail />
+              <InputPassword />
+              <LoginBtn />
+            </Box>
+            <Grid container justifyContent="space-between" pt={3}>
+              <Grid item>
+                <RegistCompanyLink setLoading={setLoading} />
+              </Grid>
+              <Grid item>
+                <SignUpLink setLoading={setLoading} />
+              </Grid>
+              <Grid item>
+                <FindPasswordLink setLoading={setLoading} />
+              </Grid>
+            </Grid>
           </Box>
-          <Grid container justifyContent="space-between">
-            <Grid item>
-              <FindPasswordLink />
-            </Grid>
-            <Grid item>
-              <SignUpLink />
-            </Grid>
-          </Grid>
-        </Box>
+        </Grid>
+        {loading && (
+          <Box
+            sx={{
+              position: 'fixed',
+              width: '100%',
+              height: '100%',
+              backgroundColor: 'rgba(0, 0, 0, 0.4)',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              zIndex: 9999,
+            }}
+          >
+            <CircularProgress color="inherit" />
+          </Box>
+        )}
       </Grid>
-    </Grid>
+    </>
   )
 }
--- front/src/components/typography/FindPasswordLink.js
+++ front/src/components/typography/FindPasswordLink.js
@@ -1,16 +1,23 @@
 import { Typography } from '@mui/material'
+import { useRouter } from 'next/navigation'
 
-const FindPasswordLink = () => {
+const FindPasswordLink = ({ setLoading }) => {
+  const router = useRouter()
+  const handleClick = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    router.push('/initial-password')
+  }
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       <a
-        href="/initial-password"
+        onClick={handleClick}
         style={{
           color: 'inherit', // 부모 Typography의 색상을 상속받습니다.
           textDecoration: 'none', // 밑줄을 제거합니다.
         }}
       >
-        {'Forgot password?'}
+        {'비밀번호 찾기'}
       </a>
     </Typography>
   )
