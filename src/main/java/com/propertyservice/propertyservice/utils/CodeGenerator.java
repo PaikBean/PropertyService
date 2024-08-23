72a5afedc93965ec4ee3062492d70237782ce797
yec3168 <yec3168@naver.com>
2024-08-23 15:44:53 +0900

    [FEAT] CodeGenerator 부서코드, 회사코드 생성.

--- src/main/java/com/propertyservice/propertyservice/utils/CodeGenerator.java
+++ src/main/java/com/propertyservice/propertyservice/utils/CodeGenerator.java
@@ -6,12 +6,33 @@ import java.util.Random;
 
 @UtilityClass
 public class CodeGenerator {
-    public String generateCompanyCode(String addressLevel1Code, String addressLevel2Code, String bizNumber){
-        Random random = new Random(Long.parseLong(bizNumber));
+
+    private static final String COM_PREFIX = "CP";
+    private static final String DEP_PREFIX ="DP";
+    private static final Integer MAX_LENGTH = 6;
+
+    public String generateCompanyCode(){
 //        Todo : 코드 생성 로직 변경
-//        1. 회사 주소의 영문 코드값 가져옴.
+//        1. CP접두사를 붙임.
+//        2. 사업자등록번호를 시드로 랜덤 번호 6자리 완성
+//        3. 완성된 회사코드 중복 검증
+        return COM_PREFIX+generateRandomNumber();
+    }
+
+    public String generateDepartmentCode(){
+//        Todo : 코드 생성 로직 변경
+//        1. DP 주소의 영문 코드값 가져옴.
 //        2. 사업자등록번호를 시드로 랜덤 번호 4자리 완성
 //        3. 완성된 회사코드 중복 검증
-        return addressLevel1Code.toUpperCase() + addressLevel2Code.toUpperCase() + random.nextInt(10000)+1;
+        return DEP_PREFIX+generateRandomNumber();
+    }
+
+    private String generateRandomNumber() {
+        StringBuilder number = new StringBuilder(MAX_LENGTH);
+        Random random = new Random();
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            number.append(random.nextInt(10)); // 0-9 사이의 랜덤 숫자 생성
+        }
+        return number.toString();
     }
 }
