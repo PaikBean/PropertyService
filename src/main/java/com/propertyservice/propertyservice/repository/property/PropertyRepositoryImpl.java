34733477b294d4d4b00e3194461306aa2e67f3d4
yec3168 <yec3168@naver.com>
2024-08-27 15:18:45 +0900

    [FEAT] 보여줄 매물 목록 회사 조건 추가.

--- src/main/java/com/propertyservice/propertyservice/repository/property/PropertyRepositoryImpl.java
+++ src/main/java/com/propertyservice/propertyservice/repository/property/PropertyRepositoryImpl.java
@@ -6,11 +6,17 @@ import com.propertyservice.propertyservice.domain.building.QOwner;
 import com.propertyservice.propertyservice.domain.common.QAddressLevel1;
 import com.propertyservice.propertyservice.domain.common.QAddressLevel2;
 import com.propertyservice.propertyservice.domain.common.TransactionType;
+import com.propertyservice.propertyservice.domain.company.QCompany;
+import com.propertyservice.propertyservice.domain.manager.Manager;
+import com.propertyservice.propertyservice.domain.manager.QManager;
 import com.propertyservice.propertyservice.domain.property.PropertyType;
 import com.propertyservice.propertyservice.domain.property.QProperty;
 import com.propertyservice.propertyservice.dto.client.QShowingPropertyCandidateDto;
 import com.propertyservice.propertyservice.dto.client.ShowingPropertyCandidateCondition;
 import com.propertyservice.propertyservice.dto.client.ShowingPropertyCandidateDto;
+import com.propertyservice.propertyservice.dto.manager.CustomUserDetail;
+import com.propertyservice.propertyservice.service.CommonService;
+import com.propertyservice.propertyservice.service.CustomUserDetailService;
 import com.propertyservice.propertyservice.utils.BooleanExpressionBuilder;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
@@ -25,6 +31,7 @@ import static org.springframework.util.StringUtils.hasText;
 @RequiredArgsConstructor
 public class PropertyRepositoryImpl implements PropertyRepositoryCustom {
     private final JPAQueryFactory queryFactory;
+    private final CommonService commonService;
 
     private final QProperty property = QProperty.property;
     private final QBuilding building = QBuilding.building;
@@ -32,10 +39,13 @@ public class PropertyRepositoryImpl implements PropertyRepositoryCustom {
     private final QBuildingAddress buildingAddress = QBuildingAddress.buildingAddress;
     private final QAddressLevel1 addressLevel1 = QAddressLevel1.addressLevel11;
     private final QAddressLevel2 addressLevel2 = QAddressLevel2.addressLevel21;
+    private final QManager manager = QManager.manager;
+    private final QCompany company = QCompany.company;
 
 
     @Override
     public List<ShowingPropertyCandidateDto> searchShowingPropertyCandidateList(ShowingPropertyCandidateCondition showingPropertyCandidateCondition) {
+        CustomUserDetail customUserDetail = commonService.getCustomUserDetailBySecurityContextHolder();
         return queryFactory
                 .select(
                         new QShowingPropertyCandidateDto(
@@ -48,7 +58,7 @@ public class PropertyRepositoryImpl implements PropertyRepositoryCustom {
                                         addressLevel2.addressLevel2,
                                         buildingAddress.addressLevel3,
                                         property.unitNumber
-                                )
+                                ).coalesce("null")
                         )
                 )
                 .from(property)
@@ -57,6 +67,9 @@ public class PropertyRepositoryImpl implements PropertyRepositoryCustom {
                 .join(buildingAddress).on(buildingAddress.eq(buildingAddress))
                 .join(addressLevel1).on(buildingAddress.addressLevel1Id.eq(addressLevel1.addressLevel1Id))
                 .join(addressLevel2).on(buildingAddress.addressLevel2Id.eq(addressLevel2.addressLevel2Id))
+                // 로그인한 회사 정보만 가져오기.
+                .join(manager).on(property.picManagerId.eq(manager.managerId))
+                .join(company).on(manager.company.eq(company).and(company.eq(customUserDetail.getCompany())))
                 .where(
                         property.transactionType.eq(TransactionType.MONTHLY).and(
                                 BooleanExpressionBuilder.orBooleanBuilder(
