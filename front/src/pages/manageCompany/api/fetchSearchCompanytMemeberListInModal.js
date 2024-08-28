05c93acc739881d0e67dd4a6f47a62f0b85380fa
JaeHyuk <jh2going@gmail.com>
2024-08-28 19:10:54 +0900

    [FIX] 부서원 목록 수정 api 화면 연결

---
+++ front/src/pages/manageCompany/api/fetchSearchCompanytMemeberListInModal.js
@@ -0,0 +1,18 @@
+import { fetchGet } from '@/utils/fetch/fetchWrapper'
+export const fetchSearchCompanytMemeberListInModal = async (companyId) => {
+    try {
+      const response = await fetchGet(
+        `/api/company/v1/manager-list/${companyId}`, {}, {}
+      )
+      console.log(response)
+      if (response.responseCode === 'SUCCESS') {
+        return response
+      } else {
+        throw new Error(response.message || 'Error!')
+      }
+    } catch (error) {
+      return new Error(error.message) // 에러 발생 시 null 반환
+    
+  }
+}
+    
